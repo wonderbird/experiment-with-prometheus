453706da4d04b17962fea3aaf4ba221437341343
Stefan Boos <kontakt@boos.systems>
2020-01-04 15:09:37 +0100

    create metrics endpoint

---
+++ sample-metrics-generator/sample-metrics-generator/Controllers/MetricsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sample_metrics_generator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MetricsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "sample 42.0";
+        }
+    }
+}
